6a09d674355984c19bf0151d9220ca8ff33bb140
Trent Johnson <trentj@efn.org>
2022-10-23 16:06:14 -0700

    add transpose of first sample

--- js/app.main.js
+++ js/app.main.js
@@ -75,7 +75,7 @@ app.main = (function() {
         notes = ['C-1','C#-1','D-1','D#-1','E-1','F-1','F#-1','G-1','G#-1','A-1','A#-1','B-1','C0','C#0','D0','D#0','E0','F0','F#0','G0','G#0','A0','A#0','B0','C1','C#1','D1','D#1','E1','F1','F#1','G1','G#1','A1','A#1','B1','C2','C#2','D2','D#2','E2','F2','F#2','G2','G#2','A2','A#2','B2','C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3','C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7','C#7','D7','D#7','E7','F7','F#7','G7','G#7','A7','A#7','B7','C8','C#8','D8','D#8','E8','F8','F#8','G8','G#8','A8','A#8','B8','C9','C#9','D9','D#9','E9','F9','F#9','G9'],
         templates = {
             menu: {
-                open: '<menu x="%1" y="%2" width="%3" height="%4" requireSelection="false" placeholderText="%5" value="1">',
+                open: '<menu x="%1" y="%2" width="%3" height="%4" requireSelection="false" placeholderText="%5" value="2">',
                 close: '</menu>'
             },
             option: {
@@ -87,7 +87,7 @@ app.main = (function() {
                 open: '<group name="%s">',
                 close: '</group>'
             },
-            sample: '<sample path="%1" loNote="%2" hiNote="%2" rootNote="%2" tags="%3" />'
+            sample: '<sample path="%1" loNote="%2" hiNote="%3" rootNote="%3" tags="%4" />'
         },
         xml = {
             options: [],
@@ -143,7 +143,8 @@ app.main = (function() {
         },
 
         populateGroups: function() {
-            let patches, midiNote, i, sample;
+            let patches, midiNote, i, sample, lowNote,
+                transpose = false;
 
             groups.forEach(function(group) {
                 patches = group.patches;
@@ -151,6 +152,7 @@ app.main = (function() {
                 patches.forEach(function(patch) {
                     if (!patch.range) {
                         patch.range = defaultRange;
+                        transpose = true;
                     }
 
                     if (!patch.path) {
@@ -166,7 +168,16 @@ app.main = (function() {
 
                     } else {
                         for (i=patch.range.start; i<=patch.range.end; i++) {
-                            sample = self.strReplace(templates.sample, [self.getPath(patch.path) + notes[i] +'.wav', midiNote, group.name]);
+                            // transpose the first sample down to F2 for default ranges
+                            if (transpose) {
+                                lowNote = midiNote - 2;
+                                transpose = false;
+
+                            } else {
+                                lowNote = midiNote;
+                            }
+
+                            sample = self.strReplace(templates.sample, [self.getPath(patch.path) + notes[i] +'.wav', lowNote, midiNote, group.name]);
                             midiNote++;
                             xml.groups.push(sample);
                         }
